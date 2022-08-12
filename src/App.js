9a2f23c4470d5a6da89b4db179cb069c549c9783
yrsolanaratonmi <romacrying@icloud.com>
2022-08-12 18:48:02 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -12,45 +12,51 @@ function App() {
          <video autoPlay loop muted className={styles.lol}>
              <source src={vid} type='video/mp4'/>
          </video>
-        <div className={styles.text}>
-            <a href='https://vk.com/mama_ubei_menya' className={styles.a} target='_blank'>
-                <Typewriter
+        <Bounce left cascade>
+            <div className={styles.text}>
+                <a href='https://vk.com/mama_ubei_menya' className={styles.a} target='_blank'>
+                    <Typewriter
 
-                    onInit={(typewriter)=> {
+                        onInit={(typewriter)=> {
 
-                        typewriter
+                            typewriter
 
-                            .typeString("yrsolanaratonmi")
+                                .typeString("yrsolanaratonmi")
 
-                            .pauseFor(1000)
-                            .start();
-                    }}
-                />
-            </a>
+                                .pauseFor(1000)
+                                .start();
+                        }}
+                    />
+                </a>
 
-        </div>
-        <div className={styles.text2}>
-            <a href='https://wwwwwwwww.jodi.org/100cc/index.html' className={styles.b} target='_blank'>
-                <Typewriter
+            </div>
+        </Bounce>
 
-                    onInit={(typewriter)=> {
+        <Bounce right cascade>
+            <div className={styles.text2}>
+                <a href='https://wwwwwwwww.jodi.org/100cc/index.html' className={styles.b} target='_blank'>
+                    <Typewriter
 
-                        typewriter
+                        onInit={(typewriter)=> {
 
-                            .typeString("I am the shipwreck with its scattered and soaked wood littering the ocean like a nest of roaches. The water smothers me patiently, and it waits to feel my body on the ocean floor. I am the captain. I stand on my ship, never leaving its side, for tonight I sleep in a shark's stomach. The tears and rain both hit my face, as I lay on the deck and wait to drown. I am the thunder. Hear me roar as I deafen all. I conduct my orchestra as loud as possible and conclude with a violent smash. I am washing the splinters from within my heart and I scream as it pumps. I let my lungs fill with water as the broken bathtub leaks.")
-                            .pauseFor(1000)
-                            .deleteAll()
-                            .typeString("The cure for suffering which is the collision of consciousness with unconsciousness is not to be submerged in unconsciousness, but to beraised to consciousness and to suffer more. The evil of suffering is cured by more suffering, by higher suffering. Do not take opium, but put salt and vinegar.\n" +
-                                "in the soul's wound, for when you sleep and no longer feel the suffering, you are not. And to be, that is imperative. Do not then close your eyes to the\n" +
-                                "agonizing Sphinx, but look her in the ace, and let her seize you in her mouth,\n" +
-                                "and crunch you with her hundred thousand poisonous teeth, and swallow\n" +
-                                "you. And when she has swallowed you, you will know the sweetness of the taste of suffering.")
-                            .start();
-                    }}
-                />
-            </a>
+                            typewriter
 
-        </div>
+                                .typeString("I am the shipwreck with its scattered and soaked wood littering the ocean like a nest of roaches. The water smothers me patiently, and it waits to feel my body on the ocean floor. I am the captain. I stand on my ship, never leaving its side, for tonight I sleep in a shark's stomach. The tears and rain both hit my face, as I lay on the deck and wait to drown. I am the thunder. Hear me roar as I deafen all. I conduct my orchestra as loud as possible and conclude with a violent smash. I am washing the splinters from within my heart and I scream as it pumps. I let my lungs fill with water as the broken bathtub leaks.")
+                                .pauseFor(1000)
+                                .deleteAll()
+                                .typeString("The cure for suffering which is the collision of consciousness with unconsciousness is not to be submerged in unconsciousness, but to beraised to consciousness and to suffer more. The evil of suffering is cured by more suffering, by higher suffering. Do not take opium, but put salt and vinegar.\n" +
+                                    "in the soul's wound, for when you sleep and no longer feel the suffering, you are not. And to be, that is imperative. Do not then close your eyes to the\n" +
+                                    "agonizing Sphinx, but look her in the ace, and let her seize you in her mouth,\n" +
+                                    "and crunch you with her hundred thousand poisonous teeth, and swallow\n" +
+                                    "you. And when she has swallowed you, you will know the sweetness of the taste of suffering.")
+                                .start();
+                        }}
+                    />
+                </a>
+
+            </div>
+        </Bounce>
+
         <div className={styles.text_block}>
             <Bounce left cascade>
                 <div>
@@ -88,39 +94,43 @@ function App() {
                 </div>
             </Bounce>
         </div>
+        <Bounce bottom cascade>
 <div className={styles.div}>
-    <div className={styles.int}>
-        <Typewriter
 
-            onInit={(typewriter)=> {
+        <div className={styles.int}>
+            <Typewriter
 
-                typewriter
+                onInit={(typewriter)=> {
 
-                    .typeString("social")
+                    typewriter
 
-                    .pauseFor(1000)
-                    .start();
-            }}
-        />
-    </div>
+                        .typeString("social")
 
-    <div className={styles.int2}>
-        <Typewriter
+                        .pauseFor(1000)
+                        .start();
+                }}
+            />
+        </div>
 
-            onInit={(typewriter)=> {
+        <div className={styles.int2}>
+            <Typewriter
 
-                typewriter
+                onInit={(typewriter)=> {
 
-                    .typeString("internet hobbies")
+                    typewriter
 
-                    .pauseFor(1000)
-                    .start();
-            }}
-        />
-    </div>
+                        .typeString("internet hobbies")
+
+                        .pauseFor(1000)
+                        .start();
+                }}
+            />
+        </div>
+
+
 
 </div>
-
+        </Bounce>
 
 
 
